120dd72b689ae084b3f3b10067ffcd5ef1ff987d
matlab-zhangjiajiang <13437269290@163.com>
2018-12-01 18:17:54 +0800

    量化
    量化

--- system_stock_financial_analysis/all_financial_information_utils.py
+++ system_stock_financial_analysis/all_financial_information_utils.py
@@ -59,9 +59,9 @@ class all_financial_infor_utils(object):
               holders_pandas = None
               if (filterdata.empty != True):
                   filterdata['date'] = currentdate
-                  print(filterdata.reset_index(drop=False,inplace=False))
+                  print(filterdata)
 
 if __name__ == '__main__':
      #all_financial_infor_utils().get_all_financial_resource()
      #all_financial_infor_utils().get_single_financial_resource('gpcw20180930.zip')
-     all_financial_infor_utils().get_current_target('002042',['EPS','turnoverRatioOfInventory','currentRatio'])
+     all_financial_infor_utils().get_current_target('001696',['EPS','turnoverRatioOfInventory','currentRatio'])
