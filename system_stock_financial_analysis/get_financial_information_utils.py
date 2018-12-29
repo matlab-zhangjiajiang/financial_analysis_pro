4a51ed249d35b96dfa61627d4eddbb5b2c75ba34
180394831 <13437269290@163.com>
2018-12-29 13:53:20 +0800

    量化
    量化

--- system_stock_financial_analysis/get_financial_information_utils.py
+++ system_stock_financial_analysis/get_financial_information_utils.py
@@ -92,6 +92,7 @@ if __name__ == '__main__':
 
      #all_financial_infor_utils().get_single_financial_resource('gpcw20180930.zip')
 
-     code = '600079'
-     columnlist= ['EPS','ROE','turnoverRatioOfInventory','currentRatio','numberOfShareholders','accountsReceivables']
+     code = '001696'
+     columnlist= ['EPS','ROE','turnoverRatioOfInventory','currentRatio','numberOfShareholders','accountsReceivables','daysSalesOutstanding','cashRatio',
+                  'goodwill','totalOwnersEquity']
      get_financial_infor_utils().get_current_target(code,columnlist)
