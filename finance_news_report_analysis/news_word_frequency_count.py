32a058fcf5d78060daccd9e4c477c3ff797d153d
180394831 <13437269290@163.com>
2019-01-09 19:11:40 +0800

    量化
    量化

--- finance_news_report_analysis/news_word_frequency_count.py
+++ finance_news_report_analysis/news_word_frequency_count.py
@@ -8,7 +8,7 @@ from collections import Counter
 from finance_common_utils.mysql_dbutils import sqlalchemy_dbutils as dbmanager
 import utils.spider_common_utils as utils
 #新增关键词
-stopwords_dict = ['一带一路', '雄安新区', '区块链', '数字货币', '虚拟货币',  '比特币', '对冲基金', '自贸区', '自由贸易区','乡村振兴','美丽中国','共享经济','租购同权','新零售',
+stopwords_dict = ['雄安新区', '区块链', '数字货币', '虚拟货币',  '比特币', '对冲基金', '自贸区', '自由贸易区','乡村振兴','美丽中国','共享经济','租购同权','新零售',
              '共有产权房','楼市调控', '产权保护', '互联网金融', '5G', '4G', '国企改革', '大湾区', '长江经济带']
 
 
@@ -28,15 +28,16 @@ def words_frequency_count():
     for idx,row in basicdata.iterrows():
         jieba.add_word(row['name'])
         jieba.add_word(idx)
-        stopwords.append(row['name'])
-        stopwords.append(idx)
+        stopwords.append(row['name'].replace(' ',''))
+        stopwords.append(idx.replace(' ',''))
 
     project_path = os.path.dirname(os.path.realpath(__file__))
     url = project_path+'\\main_word_dict.txt'
     stop_url = project_path+'\\stop_word_dict.txt'
 
+    update_idf_dicttext(url,stopwords)
     jieba.analyse.set_stop_words(stop_url)
-    #jieba.analyse.default_tfidf.median_idf
+    jieba.analyse.set_idf_path(url)
     conengine = dbmanager.sql_manager().init_engine()
     sql ='SELECT * FROM FINANCE_SYSTEM_STOCK_NEWS_DATA WHERE DATE(CREATE_TIME) = CURDATE()'
     df = pd.read_sql_query(sql,conengine)
@@ -58,9 +59,10 @@ def words_frequency_count():
 def update_idf_dicttext(url,stopwords):
     ##新的词义写入到当前的文档
     writefile = open(url, 'a')
-    #writefile.truncate()
+    writefile.truncate()
+    writefile.write('一带一路 9.85762638414')
     for adddata in stopwords:
-        writefile.write('\n'+adddata)
+        writefile.write('\n'+adddata+' 9.85762638414')
 
 if __name__ == '__main__':
     words_frequency_count()
