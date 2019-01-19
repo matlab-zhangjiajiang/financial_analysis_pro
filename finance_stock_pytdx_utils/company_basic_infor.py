328ba960ce4bd5d48b29240208b9e74c5d30b4ac
180394831 <13437269290@163.com>
2019-01-19 18:03:27 +0800

    量化
    量化

---
+++ finance_stock_pytdx_utils/company_basic_infor.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+from pytdx.hq import TdxHq_API
+from pytdx.params import TDXParams
+from connection_host_server import connection_host_server as serviceinfo
+api = TdxHq_API()
+
+def get_company_basic_infor():
+    with api.connect(serviceinfo.HOST_IP, serviceinfo.HOST_PORT):
+
