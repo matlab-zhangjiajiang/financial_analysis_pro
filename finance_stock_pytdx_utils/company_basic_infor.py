a54964cff5a94259731f720afcd4484658165e91
180394831 <13437269290@163.com>
2019-01-22 15:31:30 +0800

    量化
    量化

--- finance_stock_pytdx_utils/company_basic_infor.py
+++ finance_stock_pytdx_utils/company_basic_infor.py
@@ -6,4 +6,13 @@ api = TdxHq_API()
 
 def get_company_basic_infor():
     with api.connect(serviceinfo.HOST_IP, serviceinfo.HOST_PORT):
+      df = api.get_and_parse_block_info(TDXParams.BLOCK_SZ)
 
+      print(df)
+
+
+
+
+
+if __name__ == '__main__':
+    get_company_basic_infor()
