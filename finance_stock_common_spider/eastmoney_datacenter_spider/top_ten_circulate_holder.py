6d5a2dce7d9a298fe7d150b606afcefaf2749a29
matlab-zhangjiajiang <13437269290@163.com>
2019-07-07 10:28:09 +0800

    量化
    量化

--- finance_stock_common_spider/eastmoney_datacenter_spider/top_ten_circulate_holder.py
+++ finance_stock_common_spider/eastmoney_datacenter_spider/top_ten_circulate_holder.py
@@ -2,8 +2,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+from finance_common_utils.mysql_dbutils import sqlalchemy_dbutils as dbmanager
 from finance_stock_common_spider.eastmoney_datacenter_spider import spider_data_web_address as address
 from finance_stock_tushare_utils.stock_basic_data import stock_area_tools as areatools
+from finance_stock_dao_model.stock_circulat_holds_data_20190630_dto import stock_circulat_holds_data_20190630_dto as dto
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -18,24 +20,29 @@ class top_ten_circulate_holder(object):
         tu.pro_api()
 
     #ggdate 公告时间
-    def get_top_ten_circulate_holder(self,stockcode,ggdate):
+    def get_top_ten_circulate_holder(self,mainkey,ggdate,stockcode):
         chrome_options = Options()
         chrome_options.add_argument('--headless')  # 指定无界面形式运行
         chrome_options.add_argument('no-sandbox')  # 禁止沙盒
         driver = webdriver.Chrome(options=chrome_options)
         #获取对应的地址
-        url = address.spider_web_address().DATA_URL['TOP_TEN_CIRCULATE_HOLDER']+stockcode
+        url = address.spider_web_address().DATA_URL['TOP_TEN_CIRCULATE_HOLDER']+mainkey
         print(url)
         driver.get(url)
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'content')))
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'section')))
         #当前年份公告列表
         element = driver.find_element_by_class_name('tab')
         datavalus = element.find_elements_by_tag_name('li')
         dd_data_list =[]
+        gg_date_list = []
         for data in datavalus:
-            dd_data_list.append(data.text)
-        if ggdate in dd_data_list:
-           current_element = driver.find_element_by_id('TTCS_Table_Div')
+            gg_date_list.append(data.text)
+            dd_data_list.append(int(data.text.replace('-','')[0:6]))
+        max_report_date = max(dd_data_list)
+        max_gg_date = max(gg_date_list)
+        if max_report_date > ggdate:
+           current_element = driver.find_element_by_id('templateDiv')
+           current_element = current_element.find_element_by_id('TTS_Table_Div')
            current_table = current_element.find_element_by_css_selector("table[style='display:table']")
            li_list = current_table.find_elements_by_tag_name("tr")
            li_list = li_list[2:len(li_list)-1]
@@ -43,7 +50,6 @@ class top_ten_circulate_holder(object):
                current_data = li_value.find_elements_by_tag_name('td')
                i=0
                holder_name =''
-               holder_nature=''
                stock_type=''
                hold_amount=0
                zzltg_cg_ratio=0
@@ -53,22 +59,23 @@ class top_ten_circulate_holder(object):
                    if i==0:
                        holder_name =holder_data.text
                    elif i==1:
-                       holder_nature = holder_data.text
-                   elif i==2:
                        stock_type = holder_data.text
-                   elif i==3:
+                   elif i==2:
                        hold_amount = holder_data.text.replace(',','')
-                   elif i==4:
+                   elif i==3:
                        zzltg_cg_ratio = holder_data.text.replace('%','')
-                   elif i==5:
+                   elif i==4:
                        zj_state = holder_data.text.replace('不变','0').replace(',','')
-                   elif i==6:
+                   elif i==5:
                        bd_ratio = holder_data.text.replace('%','').replace('--','0')
                    i=i+1
-               print(holder_name+'--'+holder_nature+'--'+stock_type+'--'+hold_amount+'--'+zzltg_cg_ratio+'--'+zj_state+'--'+bd_ratio)
-               print('--------------------->')
+               id=stockcode+max_gg_date.replace('-','')+holder_name.replace('-','')
+               print(holder_name+'--'+stock_type+'--'+hold_amount+'--'+zzltg_cg_ratio+'--'+zj_state+'--'+bd_ratio)
+               crdto = dto(id=id,ts_code=stockcode,ann_date=max_gg_date,end_date=max_gg_date,holder_name=holder_name,
+                           hold_amount=hold_amount,holder_nature='',stock_type=stock_type,zzltg_cg_ratio=zzltg_cg_ratio,
+                           zj_state=zj_state,bd_ratio=bd_ratio)
+               dbmanager.sql_manager().single_common_save_basedata(crdto)
         driver.close()
-
 
 
     def spider_cron_top_ten_circulate_holder(self):
@@ -77,7 +84,7 @@ class top_ten_circulate_holder(object):
             areacode = areatools.stock_area_tools().get_stock_area_code(stokdata)
             mainkey = areacode+stokdata
             print(mainkey)
-            self.get_top_ten_circulate_holder(mainkey,'2019-03-31')
+            self.get_top_ten_circulate_holder(mainkey,201903,stokdata)
 
 
 if __name__ == '__main__':
--- finance_stock_dao_model/stock_circulat_holds_data_20190630_dto.py
+++ finance_stock_dao_model/stock_circulat_holds_data_20190630_dto.py
@@ -2,12 +2,13 @@
 from sqlalchemy import Column, Integer, String, Text ,Float,DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
 class stock_circulat_holds_data_20190630_dto(Base):
 
     __tablename__ = 'finance_system_stock_circulat_holds_data_20190630'
-
+    id = Column(String(100),primary_key=True)
     ts_code = Column(String(32))
     ann_date = Column(DateTime)
     end_date = Column(DateTime)
