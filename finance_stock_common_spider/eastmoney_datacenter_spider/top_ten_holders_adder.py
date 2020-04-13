5782c5681dd7c3cfd7987c4940a2759b15b6a0c8
matlab-zhangjiajiang <13437269290@163.com>
2020-04-13 22:16:56 +0800

    分析

---
+++ finance_stock_common_spider/eastmoney_datacenter_spider/top_ten_holders_adder.py
@@ -0,0 +1,23 @@
+#utf-8
+import requests
+import pandas as pd
+import json
+from finance_stock_common_spider.eastmoney_datacenter_spider import spider_data_web_address as address
+from finance_common_utils.mysql_dbutils import sqlalchemy_dbutils as dbmanager
+from finance_stock_dao_model.stock_dayly_increase_holder_dto import stock_dayly_increase_holder_dto as daylydata
+
+
+class dayly_add_stock_topten_holders(object):
+
+    def get_dayly_add_topten_holders_raw_data(self):
+        response = requests.get(address.spider_web_address().DATA_URL['TOP_TEN_HOLDER_ADDER'])
+        current_data = str(response.text.encode('utf-8'))
+        data = current_data.find('DlswLagO =')+11
+        message = current_data[data:-1]
+        #raw_data = json.loads(message)
+        print(message)
+
+
+
+if __name__ == '__main__':
+    dayly_add_stock_topten_holders().get_dayly_add_topten_holders_raw_data()
