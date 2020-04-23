ed87c0fff31a0b3be5f3a4431cbb2d3583a4464b
180394831 <13437269290@163.com>
2020-04-23 14:49:39 +0800

    python爬虫

--- finance_stock_common_spider/eastmoney_datacenter_spider/spider_data_web_address.py
+++ finance_stock_common_spider/eastmoney_datacenter_spider/spider_data_web_address.py
@@ -5,4 +5,4 @@ class spider_web_address(object):
       DATA_URL = {'INCREASE_USERS':'http://data.eastmoney.com/DataCenter_V3/Chart/cjsj/weeklystockaccountsnew.ashx?isxml=true',
                   'DAYLY_INCREASE_HOLDINGS': 'http://datainterface3.eastmoney.com/EM_DataCenter_V3/api/GDZC/GetGDZC?tkn=eastmoney&cfg=gdzc&secucode=&fx=1&sharehdname=&pageSize=50&pageNum=1&sortFields=NOTICEDATE&sortDirec=1&startDate=&endDate=',
                   'TOP_TEN_CIRCULATE_HOLDER':'http://emweb.securities.eastmoney.com/ShareholderResearch/Index?type=web&code=',
-                  'TOP_TEN_HOLDER_ADDER':'http://data.eastmoney.com/DataCenter_V3/gdfx/data.ashx?SortType=NDATE&SortRule=1&PageIndex=1&PageSize=100&jsObj=DlswLagO&type=NSHDDETAIL&date=&gdlx=0&cgbd=1&rt=52888792'}
+                  'TOP_TEN_HOLDER_ADDER':'http://data.eastmoney.com/gdfx/holdingDetail.aspx?cgbd=1'}
--- finance_stock_common_spider/eastmoney_datacenter_spider/top_ten_holders_new_import.py
+++ finance_stock_common_spider/eastmoney_datacenter_spider/top_ten_holders_new_import.py
@@ -1,46 +1,64 @@
 #utf-8
-import requests
-import json
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from finance_common_utils.mysql_dbutils import sqlalchemy_dbutils as dbmanager
+from finance_stock_common_spider.eastmoney_datacenter_spider import spider_data_web_address as address
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from finance_stock_common_spider.eastmoney_datacenter_spider import spider_data_web_address as address
 from finance_common_utils.mysql_dbutils import sqlalchemy_dbutils as dbmanager
 from finance_stock_dao_model.stock_topten_holders_new_import_dto import current_table_dto  as daylydata
+from finance_common_utils.common_utils import Logger as loggers
 
+logger = loggers.Logger(logname='log.txt', loglevel=1, logger="main_job").getlog()
 
 class top_ten_holders_new_import(object):
 
     def get_dayly_add_topten_holders_raw_data(self):
-        response = requests.get(address.spider_web_address().DATA_URL['TOP_TEN_HOLDER_ADDER'])
-        current_data = str(response.text)
-        data = current_data.find('DlswLagO =')+11
-        message = current_data[data:-1].replace('pages','\"pages\"').replace('data','\"data\"')
-        raw_data = json.loads(message)
-        data = raw_data['data']
-        for cdata in data:
-            company_code = cdata['COMPANYCODE']
-            ssname = cdata['SSNAME']
-            sharehdname = cdata['SHAREHDNAME']
-            sharehdtype = cdata['SHAREHDTYPE']
-            sharestype = cdata['SHARESTYPE']
-            rank = cdata['RANK']
-            scode = cdata['SCODE']
-            sname = cdata['SNAME']
-            rdate = cdata['RDATE'].replace('T',' ')
-            sharehdnum = cdata['SHAREHDNUM']
-            ltag = cdata['LTAG']
-            zb = cdata['ZB']
-            ndate = cdata['NDATE'].replace('T',' ')
-            bz = cdata['BZ']
-            bdbl = cdata['BDBL']
-            sharehdcode = cdata['SHAREHDCODE']
-            sharehdratio =cdata['SHAREHDRATIO']
-            bdsum = cdata['BDSUM']
-            mainkey = sharehdcode
-            vo = daylydata(mainkey=mainkey, company_code=company_code, ssname=ssname, sharehdname=sharehdname,
-                           sharehdtype=sharehdtype, sharestype=sharestype, rank=rank, scode=scode,
-                           sname=sname, rdate=rdate, sharehdnum=sharehdnum,
-                           ltag=ltag, zb=zb, ndate=ndate,
-                           bz=bz, bdbl=bdbl, sharehdcode=sharehdcode,sharehdratio=sharehdratio,bdsum=bdsum)
-            dbmanager.sql_manager().single_common_save_basedata(vo)
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')  # 指定无界面形式运行
+        chrome_options.add_argument('no-sandbox')  # 禁止沙盒
+        driver = webdriver.Chrome(options=chrome_options)
+        try:
+            # 获取对应的地址
+            url = address.spider_web_address().DATA_URL['TOP_TEN_HOLDER_ADDER']
+            driver.get(url)
+            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'tb_cgmx')))
+            # 当前每天股东新增
+            element = driver.find_element_by_id('tb_cgmx')
+            tbody = element.find_element_by_tag_name('tbody')
+            trdata = tbody.find_elements_by_tag_name('tr')
+            for tr in trdata:
+                cdata = tr.text.replace('- - ','')
+                cdatas = cdata.split('\n')
+                sharehdname = cdatas[1]
+                cur_tr_data = cdatas[2]
+                datas = cur_tr_data.split(' ')
+                logger.info(datas)
+                sharehdtype = datas[0]
+                rank =datas[1]
+                scode = datas[2]
+                sname = datas[3]
+                rdate = datas[7]
+                sharehdnum = str(datas[8]).replace(',','')
+                zb = datas[9]
+                bz = datas[10]
+                ltsz = str(datas[11]).replace(',','')
+                ndate = datas[12]
+                mainkey = sharehdname+ndate+scode
+                vo = daylydata(mainkey=mainkey,sname=sname, sharehdname=sharehdname,
+                           sharehdtype=sharehdtype,  rank=rank, scode=scode,
+                           rdate=rdate, sharehdnum=sharehdnum, zb=zb, ndate=ndate,
+                           bz=bz,ltsz=ltsz)
+                dbmanager.sql_manager().single_common_save_basedata(vo)
+        except Exception as e:
+            logger.info('执行出错!', e)
+        finally:
+            driver.close()
+
+
 
 
 
